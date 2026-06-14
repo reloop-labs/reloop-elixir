e63ddb6f84590cd96a05e97a6a3dc935482932f0
twinkalp10 <twinkal.work@outlook.com>
2026-06-15 00:36:23 +0530

    chore: bump project version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Reloop.MixProject do
   def project do
     [
       app: :reloop,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
